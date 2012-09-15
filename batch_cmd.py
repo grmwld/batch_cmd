e3ce33fba66cd8115707adbb4cfd0cd2036a7afd
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2012-09-15 10:14:55 +0200

    Use Queue to dispatch logs with ERROR level

--- batch_cmd.py
+++ batch_cmd.py
@@ -6,8 +6,18 @@ import sys
 import argparse
 import logging
 import multiprocessing
+import Queue
 import time
 from multiworkers.multiworker import Controller, Worker
+
+
+class QueueHandler(logging.Handler):
+    def __init__(self, queue):
+        logging.Handler.__init__(self)
+        self.queue = queue
+
+    def emit(self, record):
+        self.queue.put(record)
 
 
 class MyWorker(Worker):
@@ -29,6 +39,40 @@ class MyWorker(Worker):
 class MyController(Controller):
     def __init__(self, *args, **kwargs):
         Controller.__init__(self, *args, **kwargs)
+        self.global_params['error_logs_queue'] = multiprocessing.Queue()
+        self.error_logs = []
+        logger = logging.getLogger(
+            self.global_params['infile'].name.replace('/', '').strip('.')
+        )
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        file_handler = logging.StreamHandler(self.global_params['logfile'])
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        console_handler = QueueHandler(self.global_params['error_logs_queue'])
+        console_handler.setLevel(logging.ERROR)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        self.global_params['logger'] = logger
+
+    def update_error_logs(self):
+        while True:
+            try:
+                self.error_logs.append(
+                    str(self.global_params['error_logs_queue'].get_nowait())
+                )
+            except Queue.Empty:
+                return self.error_logs
+
+    def update_progress_message(self):
+        msg = '\n\n'.join([
+            Controller.update_progress_message(self).rstrip(),
+            '\n'.join(self.update_error_logs())
+        ])
+        return msg
 
     def finish(self):
         output = ', '.join(map(str, [c['oc'] for c in self.results]))
@@ -36,23 +80,12 @@ class MyController(Controller):
 
 
 def main(args):
-    logger = logging.getLogger(args.infile.name.replace('/', '').strip('.'))
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler = logging.StreamHandler(args.logfile)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(formatter)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
     c = MyController(
         jobs=({'cmd': l.strip()} for l in args.infile),
         global_params={
+            'infile': args.infile,
             'outfile': args.outfile,
-            'logger': logger
+            'logfile': args.logfile
         },
         num_cpu=args.num_cpu,
         quiet=args.quiet,
